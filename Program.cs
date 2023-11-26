8773be31d27f9b7609f240f470079f85d31d2be3
Nicole <rosanik3103@gmail.com>
2023-11-26 01:58:49 -0300

    adciona chatbot

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string sair = "tchau";
+string pergunta;
+
+
+Console.WriteLine
+    ("--- ChatBot Groot ---");
+Console.WriteLine
+    ("Digite tchau quando quiser sair\n");
+
+do
+{
+    Console.Write("Pergunta: ");
+    pergunta = Console.ReadLine()!;
+    Console.WriteLine("            Resposta: Eu sou Groot.");
+
+    if(pergunta == sair)
+    {
+        Console.WriteLine("Eu sou o Groot!");
+    }
+}
+while(pergunta != sair);
